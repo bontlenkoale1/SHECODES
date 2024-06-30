57db4190e710170ec7e96aa77d1853d101ddf02d
Bontle <126960560+bontlenkoale1@users.noreply.github.com>
2024-06-30 22:37:04 +0200

    SheCodes Basic

---
+++ SheCodes Basic 👩🏾‍💻/learn javascript.js
@@ -0,0 +1,10 @@
+let pizza
+//undefined 
+function orderPizza() {
+    console.log('order pizza')
+    pizza = 'pizza'
+    console.log('pizza was ordered')
+}
+orderPizza(
+    console.log('Eat ${pizza}')
+)
