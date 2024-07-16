b5bdacc3a0aa0e690c837478f0038327d0f9cfc3
Bontle <126960560+bontlenkoale1@users.noreply.github.com>
2024-07-16 20:45:28 +0200

    JS Arrays vs Objects🌗

---
+++ SheCodes Plus 👩🏾‍💻/javascript-arrays-vs-objects-challenge-forked/src/index.js
@@ -0,0 +1,67 @@
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true,
+};
+console.log("Temperature:", paris.temperature);
+
+//Challenge 2
+
+let sydney = {
+  name: "Sydney",
+  country: "Australia",
+  language: "English",
+  temperature: 20,
+  capitalCity: true,
+};
+console.log("Temperature:", sydney.temperature);
+
+//Challange 3
+let australianCities = [
+  {
+    name: "Sydney",
+  },
+  {
+    name: "NewCastle",
+  },
+  {
+    name: "Liverpool",
+  },
+];
+console.log(australianCities[0].name);
+console.log(australianCities[1].name);
+console.log(australianCities[2].name);
+
+//Challange 4
+
+let cities = [
+  {
+    name: "Johannesburg",
+    country: "South Africa",
+    language: "English",
+    temperature: 18,
+    capitalCity: true,
+  },
+  {
+    name: "Seoul",
+    country: "South Korea",
+    language: "Korean",
+    temperature: 27,
+    capitalCity: true,
+  },
+  {
+    name: "Tokyo",
+    country: "Japan",
+    language: "Japanese",
+    temperature: 24,
+    capitalCity: true,
+  },
+];
+console.log(cities[0].temperature);
+console.log(cities[0].name);
+console.log(cities[1].temperature);
+console.log(cities[1].name);
+console.log(cities[2].temperature);
+console.log(cities[2].name);
