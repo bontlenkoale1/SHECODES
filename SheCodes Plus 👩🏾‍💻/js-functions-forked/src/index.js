90f7cbcc57764c28aef823909c8ea8fa99ea3d9e
Bontle <126960560+bontlenkoale1@users.noreply.github.com>
2024-07-18 17:31:52 +0200

    javascript functions 🎰

---
+++ SheCodes Plus 👩🏾‍💻/js-functions-forked/src/index.js
@@ -0,0 +1,13 @@
+// Challenge
+// 1. Create a function isWindy and alert 'It is windy'
+// 2. Call the function twice
+function isWindy() {
+  let windspeed = 80;
+  if (windspeed < 70) {
+    alert("It is not windy");
+  } else {
+    alert("It is windy!🌬️");
+  }
+}
+isWindy();
+isWindy();
