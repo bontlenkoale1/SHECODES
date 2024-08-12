095a31bbb0ba399109a5e86d7a3ac5db13b5c0ab
Bontle <126960560+bontlenkoale1@users.noreply.github.com>
2024-08-12 16:01:47 +0200

    JS Functions Parameters🐝

---
+++ SheCodes Plus 👩🏾‍💻/js-functions-params-forked/src/index.js
@@ -0,0 +1,16 @@
+// Challenge
+// 1. Create a function isWindy
+// 2. Add a speed parameter to the function
+// 3. If speed is greater than 5, alert ‘It is windy’
+// 4. Else, alert ‘It is not windy’
+// 5. Call the function twice with different arguments to test both scenarios
+function isWindy(speed) {
+  if (speed > 5) {
+    alert("It is windy🌬️");
+  } else {
+    alert("It is not windy!");
+  }
+}
+let currentSpeed = 5;
+isWindy(currentSpeed);
+isWindy(20);
